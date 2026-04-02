4b3bf2f5f570de228ac1522c4a68578442951de6
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-04-02 00:51:29 -0400

    Se agregó el Mapper de MensajeSoporte.

---
+++ app/src/main/java/ucne/edu/elitecut/data/mapper/MensajeSoporteMappers.kt
@@ -0,0 +1,49 @@
+package ucne.edu.elitecut.data.mapper
+
+import ucne.edu.elitecut.data.local.entities.MensajeSoporteEntity
+import ucne.edu.elitecut.data.remote.dtos.MensajeResponseDto
+import ucne.edu.elitecut.domain.model.MensajeSoporte
+
+fun MensajeSoporteEntity.toDomain() = MensajeSoporte(
+    id = id,
+    remoteId = remoteId,
+    usuarioId = usuarioId,
+    contenido = contenido,
+    estado = estado,
+    tipoMensaje = tipoMensaje,
+    nombreUsuario = nombreUsuario,
+    fechaEnvio = fechaEnvio,
+    isPendingCreate = isPendingCreate
+)
+
+fun MensajeSoporte.toEntity() = MensajeSoporteEntity(
+    id = id,
+    remoteId = remoteId,
+    usuarioId = usuarioId,
+    contenido = contenido,
+    estado = estado,
+    tipoMensaje = tipoMensaje,
+    nombreUsuario = nombreUsuario,
+    fechaEnvio = fechaEnvio,
+    isPendingCreate = isPendingCreate
+)
+
+fun MensajeResponseDto.toEntity() = MensajeSoporteEntity(
+    remoteId = id,
+    usuarioId = "",
+    contenido = contenido,
+    estado = estado,
+    tipoMensaje = tipoMensaje,
+    nombreUsuario = nombreUsuario,
+    fechaEnvio = fechaEnvio
+)
+
+fun MensajeResponseDto.toDomain(): MensajeSoporte = MensajeSoporte(
+    remoteId = id,
+    contenido = contenido,
+    estado = estado,
+    tipoMensaje = tipoMensaje,
+    nombreUsuario = nombreUsuario,
+    fechaEnvio = fechaEnvio,
+    respuestas = respuestas?.map { it.toDomain() } ?: emptyList()
+)
