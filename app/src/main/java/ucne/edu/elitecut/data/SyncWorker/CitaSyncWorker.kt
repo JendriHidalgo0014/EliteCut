102354054b166d7f542ad6b012e34cfb81d49bc9
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-04-02 01:14:55 -0400

    Se agregó el SyncWorker de citas.

---
+++ app/src/main/java/ucne/edu/elitecut/data/SyncWorker/CitaSyncWorker.kt
@@ -0,0 +1,26 @@
+package ucne.edu.elitecut.data.syncworker
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import ucne.edu.elitecut.data.remote.Resource
+import ucne.edu.elitecut.domain.repository.CitaRepository
+
+@HiltWorker
+class CitaSyncWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val citaRepository: CitaRepository
+) : CoroutineWorker(context, workerParams) {
+
+    override suspend fun doWork(): Result {
+        return when (citaRepository.postPendingCitas()) {
+            is Resource.Success -> Result.success()
+            is Resource.Error -> Result.retry()
+            else -> Result.retry()
+        }
+    }
+}
