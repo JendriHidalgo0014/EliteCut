33c12ad291b7fbb48ccc704a4caafacbbf5bd3f2
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-04-06 22:42:27 -0400

    Se agregó el ViewModel de Home.

---
+++ app/src/main/java/ucne/edu/elitecut/presentation/tareas/clientes/clienteHome/HomeViewModel.kt
@@ -0,0 +1,61 @@
+package ucne.edu.elitecut.presentation.tareas.clientes.clienteHome
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import ucne.edu.elitecut.data.remote.Resource
+import ucne.edu.elitecut.domain.usecase.BarberoUseCase.ObserveBarberosUseCase
+import ucne.edu.elitecut.domain.usecase.BarberoUseCase.SyncBarberosUseCase
+import ucne.edu.elitecut.domain.usecase.BarberoUseCase.TriggerBarberoSyncUseCase
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val observeBarberosUseCase: ObserveBarberosUseCase,
+    private val syncBarberosUseCase: SyncBarberosUseCase,
+    private val triggerBarberoSyncUseCase: TriggerBarberoSyncUseCase
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(HomeUiState(isLoading = true))
+    val state: StateFlow<HomeUiState> = _state.asStateFlow()
+
+    init {
+        observeBarberos()
+        syncFromApi()
+    }
+
+    fun onEvent(event: HomeUiEvent) {
+        when (event) {
+            is HomeUiEvent.OnSearchChange -> _state.update { it.copy(searchQuery = event.query) }
+            is HomeUiEvent.LoadBarberos -> syncFromApi()
+            is HomeUiEvent.UserMessageShown -> _state.update { it.copy(userMessage = null) }
+        }
+    }
+
+    private fun observeBarberos() = viewModelScope.launch {
+        observeBarberosUseCase().collect { barberos ->
+            _state.update {
+                it.copy(
+                    isLoading = false,
+                    barberos = barberos
+                )
+            }
+        }
+    }
+
+    private fun syncFromApi() = viewModelScope.launch {
+        when (val result = syncBarberosUseCase()) {
+            is Resource.Error -> {
+                _state.update { it.copy(isLoading = false, userMessage = result.message) }
+            }
+            else -> {
+                _state.update { it.copy(isLoading = false) }
+            }
+        }
+    }
+}
