f8c5b95ca7600c9ccd3e67ceac60f5f9f5ea83f5
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-04-13 15:38:56 -0400

    Se eliminó el metodo para cancelar citas.

--- app/src/main/java/ucne/edu/elitecut/presentation/tareas/clientes/detalleCita/DetalleCitaUiEvent.kt
+++ app/src/main/java/ucne/edu/elitecut/presentation/tareas/clientes/detalleCita/DetalleCitaUiEvent.kt
@@ -1,6 +1,5 @@
 package ucne.edu.elitecut.presentation.tareas.clientes.detalleCita
 
 sealed interface DetalleCitaUiEvent {
-    object CancelarCita : DetalleCitaUiEvent
     object UserMessageShown : DetalleCitaUiEvent
 }
